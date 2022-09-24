9ef9e9713f44656ff98644a08364d7d8303efa4a
Mahabub Islam Prio <miprio101@gmail.com>
2022-09-24 22:36:41 +0600

    init: first commit

---
+++ src/main/java/com/goodwood/maker/MakerApplication.java
@@ -0,0 +1,13 @@
+package com.goodwood.maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodwood/maker/MakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodwood.maker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
